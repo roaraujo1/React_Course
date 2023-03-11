4b8c64b3f08865b6fa750386c97ab4ef12ed7134
Rosario <Rosario@Rosario-MacBook-Pro.local>
2023-03-10 17:32:50 -0800

    using props to pass data

--- Tutorial_Instructions/03-react-basics/src/components/ExpenseItem.js
+++ Tutorial_Instructions/03-react-basics/src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const amount = 29.89;
   return (
     <div className="expense-item">
-      <div>March 3, 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__amount">$265.89</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
